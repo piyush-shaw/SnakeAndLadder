956acae83b6374cc94140e9679de3fb962d5d141
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-06 20:08:07 +0530

    checking option for player - noplay,ladder,snake

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,5 +1,8 @@
 ﻿internal class Program
 {
+    public const int NO_PLAY = 0;
+    public const int LADDER = 1;
+    public const int SNAKE = 2;
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Snake And Ladder Program");
@@ -9,6 +12,24 @@
         //UC2 Roll the dice to get a number
         Random random = new Random();
         int die = random.Next(1, 7);
-        Console.WriteLine("After rolling the die Single Player position "+die);
+        Console.WriteLine("After rolling the die Player position "+die);
+        //UC3 Check option for No Play,Ladder,Snake
+        Random random1 = new Random();
+        int option = random1.Next(0, 3);
+        if (option == NO_PLAY)
+        {
+            Console.WriteLine("Player is at same position (NO PLAY)");
+        }
+        else if (option == LADDER)
+        {
+            position = position + die;
+            Console.WriteLine("After getting the ladder player position is "+position);
+        }
+        else
+        {
+            position = die - position;
+            Console.WriteLine("After snake bite player position is "+position);
+        }
+
     }
 }
