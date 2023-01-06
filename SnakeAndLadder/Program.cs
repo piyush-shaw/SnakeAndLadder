0b8da973bf801a9c8fcf9aae38a3221a1f876253
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-06 18:35:14 +0530

    initializing the single player starting position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -3,5 +3,8 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Snake And Ladder Program");
+        //UC1 initializing the player position
+        int position = 0;
+        Console.WriteLine("Single Player starting position : " + position);
     }
 }
