f0911bbf3fe37b9d2c5aa636308d9d4dbda489f9
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-06 19:13:05 +0530

    player roll the die to get a number

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,5 +6,9 @@
         //UC1 initializing the player position
         int position = 0;
         Console.WriteLine("Single Player starting position : " + position);
+        //UC2 Roll the dice to get a number
+        Random random = new Random();
+        int die = random.Next(1, 7);
+        Console.WriteLine("After rolling the die Single Player position "+die);
     }
 }
