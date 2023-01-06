152715f3120d7bc003acdfdb0557c50c95273598
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-06 20:34:55 +0530

    Repeating till player reaches wining position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,27 +9,32 @@
         //UC1 initializing the player position
         int position = 0;
         Console.WriteLine("Single Player starting position : " + position);
-        //UC2 Roll the dice to get a number
-        Random random = new Random();
-        int die = random.Next(1, 7);
-        Console.WriteLine("After rolling the die Player position "+die);
-        //UC3 Check option for No Play,Ladder,Snake
-        Random random1 = new Random();
-        int option = random1.Next(0, 3);
-        if (option == NO_PLAY)
+        while (position < 100)
         {
-            Console.WriteLine("Player is at same position (NO PLAY)");
+            //UC2 Roll the dice to get a number
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            //UC3 Check option for No Play,Ladder,Snake
+            Random random1 = new Random();
+            int option = random1.Next(0, 3);
+            if (option == NO_PLAY)
+            {
+                Console.WriteLine("Player is at same position (NO PLAY)");
+            }
+            else if (option == LADDER)
+            {
+                position = position + die;
+                Console.WriteLine("(LADDER) After getting the ladder player position is " + position);
+            }
+            else
+            {
+                position = position - die;
+                if (position < 0)
+                {
+                    position = 0;
+                }
+                Console.WriteLine("(SNAKE) After snake bite player position is " + position);
+            }
         }
-        else if (option == LADDER)
-        {
-            position = position + die;
-            Console.WriteLine("After getting the ladder player position is "+position);
-        }
-        else
-        {
-            position = die - position;
-            Console.WriteLine("After snake bite player position is "+position);
-        }
-
     }
 }
